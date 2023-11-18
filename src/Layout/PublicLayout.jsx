b842a169dc266787e6b44b76764a948b8a623bf7
juliandebra <julian.debra25@gmail.com>
2023-11-18 17:21:02 -0300

    Fixes

--- src/Layout/PublicLayout.jsx
+++ src/Layout/PublicLayout.jsx
@@ -3,8 +3,6 @@ import Navbar from '../Components/Navbar'
 import { Outlet } from 'react-router-dom'
 import Footer from '../Components/Footer'
 import NoteList from '../Pages/NoteList'
-import { useNoteContext } from '../Context/NoteContext'
-import Loading from '../Components/Loading'
 
 const PublicLayout = () => {
 
